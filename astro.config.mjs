92891169d0710ce18c5fe49eb960e353be18b34d
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-12-29 14:59:08 +0900

    Revert "パッケージも撤去"
    This reverts commit b2386888ad9505a5dd8ed170c4fc13e562e35641.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
-// import mdx from "@astrojs/mdx";
-// import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-// import robotsTxt from "astro-robots-txt";
-// import AutoImport from 'astro-auto-import';
-// import rehypeWrapAll from 'rehype-wrap-all';
-// import rehypeUnwrapImages from 'rehype-unwrap-images';
+import robotsTxt from "astro-robots-txt";
+import AutoImport from 'astro-auto-import';
+import rehypeWrapAll from 'rehype-wrap-all';
+import rehypeUnwrapImages from 'rehype-unwrap-images';
 
 // https://astro.build/config
 export default defineConfig({
