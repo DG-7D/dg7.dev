7f80cdda463f057636c3846160ce8e26d629fde0
DG-7D <96519093+DG-7D@users.noreply.github.com>
2025-12-29 14:59:15 +0900

    Revert "色々撤去"
    This reverts commit bcd722d10be3331e352c51002487fc9be96edadf.

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,28 +10,28 @@ import rehypeUnwrapImages from 'rehype-unwrap-images';
 // https://astro.build/config
 export default defineConfig({
   site: "https://dg7.dev/",
-  // markdown: {
-  //   shikiConfig: {
-  //     theme: "dark-plus",
-  //     wrap: true,
-  //   },
-  //   rehypePlugins: [
-  //     [rehypeWrapAll, { selector: "table", wrap: "div" }],
-  //     rehypeUnwrapImages,
-  //   ],
-  // },
-  // integrations: [
-  //   AutoImport({
-  //     imports: [
-  //       "@/components/OGPCard.astro",
-  //       "@/components/Tweet.astro",
-  //     ]
-  //   }),
-  //   mdx(),
-  //   sitemap(),
-  //   robotsTxt(),
-  // ],
-  // prefetch: {
-  //   prefetchAll: true,
-  // },
+  markdown: {
+    shikiConfig: {
+      theme: "dark-plus",
+      wrap: true,
+    },
+    rehypePlugins: [
+      [rehypeWrapAll, { selector: "table", wrap: "div" }],
+      rehypeUnwrapImages,
+    ],
+  },
+  integrations: [
+    AutoImport({
+      imports: [
+        "@/components/OGPCard.astro",
+        "@/components/Tweet.astro",
+      ]
+    }),
+    mdx(),
+    sitemap(),
+    robotsTxt(),
+  ],
+  prefetch: {
+    prefetchAll: true,
+  },
 });
